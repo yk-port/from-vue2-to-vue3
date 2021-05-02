2678e14cf112714ed0af4b2c7dffbb22fdb1ca3e
tatsuya <56747371+yk-port@users.noreply.github.com>
2021-05-02 11:42:00 +0900

    initial commit

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,5 +1,8 @@
 <template>
+  <label>{{ label }}</label>
+  <!-- 親コンポーネントから渡ってくる内容は全てv-bind="$attrs"に内包される -->
   <input
+    v-bind="$attrs"
     :value="modelValue"
     @input="$emit('update:modelValue', $event.target.value)"
   />
@@ -7,30 +10,20 @@
 
 <script>
 export default {
-  name: "BaseInput",
+  // $attrsを使うときの決り文句的な感じで覚えとく
+  inheritAttrs: false,
   props: {
     modelValue: {
       type: [String, Number],
       default: "",
+    },
+    label: {
+      type: String,
+      default: null,
     },
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped></style>
